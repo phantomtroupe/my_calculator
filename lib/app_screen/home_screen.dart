669a9675abfe078178aca20b43ab2e95bebfff7f
phantomtroupe <paksiaji.1@gmail.com>
2019-04-21 03:52:40 -0700

    Bikin fitur untuk tombol AC, Backspace, sama Plus-Minus

--- lib/app_screen/home_screen.dart
+++ lib/app_screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -6,6 +7,154 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  double memory, result;
+  String currentOperation, history;
+
+  @override
+  void initState() {
+    super.initState();
+    memory = 0.0;
+    result = 0.0;
+    currentOperation = "";
+    history = "";
+  }
+
+  void btnNumberAndOperationClick(String param) {
+    if (currentOperation == "") {
+      if (param != "+" && param != "/" && param != "x" && param != "-") {
+        setState(() {
+          currentOperation += param;
+        });
+      }
+    } else {
+      String lastChar = currentOperation == ""
+          ? ""
+          : currentOperation[currentOperation.length - 1];
+      if (currentOperation.length == 1) {
+        setState(() {
+          currentOperation += param;
+        });
+      } else if (lastChar == "x" ||
+          lastChar == "-" ||
+          lastChar == "+" ||
+          lastChar == "/") {
+        if (param != "x" && param != "-" && param != "+" && param != "/") {
+          setState(() {
+            currentOperation += param;
+          });
+        } else {
+          String newString =
+              currentOperation.substring(0, currentOperation.length - 1) +
+                  param;
+          setState(() {
+            currentOperation = newString;
+          });
+        }
+      } else if (param != ".") {
+        setState(() {
+          currentOperation += param;
+        });
+      } else {
+        if (lastChar != "." && !currentOperation.contains(".")) {
+          setState(() {
+            currentOperation += param;
+          });
+        }
+      }
+    }
+  }
+
+  void btnAcclick() {
+    setState(() {
+      currentOperation = "";
+      history = "";
+    });
+  }
+
+  void btnBackSpaceClisk() {
+    if (currentOperation.length > 1) {
+      setState(() {
+        currentOperation =
+            currentOperation.substring(0, currentOperation.length - 1);
+      });
+    } else {
+      if (history != "") {
+        setState(() {
+          currentOperation = history;
+          history = "";
+        });
+      } else {
+        setState(() {
+          currentOperation = "";
+        });
+      }
+    }
+  }
+
+  // void shuntingYardAlgorithm(){
+  //   List<String> output = new List<String>();
+  //   List<String> symbol = new List<String>();
+  //   String input = "2x-3+4";
+
+  //   for(int i = 0; i < input.length; i++){
+  //     if(isNumeric(input[i])){
+  //       output.add(input[i]);
+  //     }else{
+  //       if(input[i] == "-"){
+  //         if(i == 0){
+  //           output.add("u"+input[i+1]);
+  //           i+=1;
+  //         }else{
+  //           if(!isNumeric(input[i-1])){
+  //             output.add("u"+input[i+1]);
+  //             i+=1;
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+
+  // bool isNumeric(String s){
+  //   if(s==null){
+  //     return null;
+  //   }
+  //   return double.parse(s, (e)=>null) != null;
+  // }
+
+  void btnPlusMinus() {
+    if (currentOperation != "") {
+      if (currentOperation.length == 1) {
+        currentOperation = "(-$currentOperation)";
+      } else {
+        if (currentOperation[currentOperation.length - 1] == ")") {
+          List<String> number = new List<String>();
+          for (int i = currentOperation.length - 2; i > 0; i--) {
+            if (currentOperation[i] != "-") {
+              number.add(currentOperation[i]);
+            } else {
+              break;
+            }
+          }
+          currentOperation = currentOperation.substring(
+                  0, currentOperation.length - number.length - 3) +
+              number.reversed.join();
+        } else {
+          List<String> number = new List<String>();
+          for (int i = currentOperation.length - 1; i > 0; i--) {
+            if (currentOperation[i] != "-" && currentOperation[i] != "+" && currentOperation[i] != "*" && currentOperation[i] != "/") {
+              number.add(currentOperation[i]);
+            } else {
+              break;
+            }
+          }
+          currentOperation = "(-${currentOperation.substring(0,currentOperation.length - number.length)}${number.reversed.join()})";
+        }
+      }
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -36,7 +185,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   reverse: true,
                   scrollDirection: Axis.horizontal,
                   child: Text(
-                    "",
+                    currentOperation,
                     style: TextStyle(fontSize: 45.0),
                   ),
                 )),
@@ -124,7 +273,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               right: BorderSide(
                                   width: 1.0, color: Colors.grey[300]))),
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          btnAcclick();
+                        },
                         child: Text("AC",
                             style: TextStyle(
                                 fontSize: 35.0, color: Colors.orange)),
@@ -141,7 +292,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               right: BorderSide(
                                   width: 1.0, color: Colors.grey[300]))),
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          btnBackSpaceClisk();
+                        },
                         child: Icon(
                           Icons.backspace,
                           color: Colors.orange,
@@ -160,7 +313,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               right: BorderSide(
                                   width: 1.0, color: Colors.grey[300]))),
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          btnPlusMinus();
+                        },
                         child: Text("+/-", style: TextStyle(fontSize: 35.0)),
                       ),
                     ),
@@ -175,7 +330,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               right: BorderSide(
                                   width: 1.0, color: Colors.grey[300]))),
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          btnNumberAndOperationClick("/");
+                        },
                         child: Text("/", style: TextStyle(fontSize: 35.0)),
                       ),
                     ),
@@ -196,7 +353,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               right: BorderSide(
                                   width: 1.0, color: Colors.grey[300]))),
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          btnNumberAndOperationClick("7");
+                        },
                         child: Text("7", style: TextStyle(fontSize: 35.0)),
                       ),
                     ),
@@ -210,7 +369,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               right: BorderSide(
                                   width: 1.0, color: Colors.grey[300]))),
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          btnNumberAndOperationClick("8");
+                        },
                         child: Text("8", style: TextStyle(fontSize: 35.0)),
                       ),
                     ),
@@ -224,7 +385,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               right: BorderSide(
                                   width: 1.0, color: Colors.grey[300]))),
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          btnNumberAndOperationClick("9");
+                        },
                         child: Text("9", style: TextStyle(fontSize: 35.0)),
                       ),
                     ),
@@ -239,7 +402,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               right: BorderSide(
                                   width: 1.0, color: Colors.grey[300]))),
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          btnNumberAndOperationClick("x");
+                        },
                         child: Text("x", style: TextStyle(fontSize: 35.0)),
                       ),
                     ),
@@ -260,7 +425,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               right: BorderSide(
                                   width: 1.0, color: Colors.grey[300]))),
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          btnNumberAndOperationClick("4");
+                        },
                         child: Text("4", style: TextStyle(fontSize: 35.0)),
                       ),
                     ),
@@ -274,7 +441,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               right: BorderSide(
                                   width: 1.0, color: Colors.grey[300]))),
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          btnNumberAndOperationClick("5");
+                        },
                         child: Text("5", style: TextStyle(fontSize: 35.0)),
                       ),
                     ),
@@ -288,7 +457,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               right: BorderSide(
                                   width: 1.0, color: Colors.grey[300]))),
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          btnNumberAndOperationClick("6");
+                        },
                         child: Text("6", style: TextStyle(fontSize: 35.0)),
                       ),
                     ),
@@ -303,7 +474,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               right: BorderSide(
                                   width: 1.0, color: Colors.grey[300]))),
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          btnNumberAndOperationClick("-");
+                        },
                         child: Text("-", style: TextStyle(fontSize: 35.0)),
                       ),
                     ),
@@ -324,7 +497,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               right: BorderSide(
                                   width: 1.0, color: Colors.grey[300]))),
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          btnNumberAndOperationClick("1");
+                        },
                         child: Text("1", style: TextStyle(fontSize: 35.0)),
                       ),
                     ),
@@ -338,7 +513,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               right: BorderSide(
                                   width: 1.0, color: Colors.grey[300]))),
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          btnNumberAndOperationClick("2");
+                        },
                         child: Text("2", style: TextStyle(fontSize: 35.0)),
                       ),
                     ),
@@ -352,7 +529,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               right: BorderSide(
                                   width: 1.0, color: Colors.grey[300]))),
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          btnNumberAndOperationClick("3");
+                        },
                         child: Text("3", style: TextStyle(fontSize: 35.0)),
                       ),
                     ),
@@ -367,7 +546,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               right: BorderSide(
                                   width: 1.0, color: Colors.grey[300]))),
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          btnNumberAndOperationClick("+");
+                        },
                         child: Text("+", style: TextStyle(fontSize: 35.0)),
                       ),
                     ),
@@ -389,7 +570,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               right: BorderSide(
                                   width: 1.0, color: Colors.grey[300]))),
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          btnNumberAndOperationClick("0");
+                        },
                         child: Text("0", style: TextStyle(fontSize: 35.0)),
                       ),
                     ),
@@ -403,7 +586,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               right: BorderSide(
                                   width: 1.0, color: Colors.grey[300]))),
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          btnNumberAndOperationClick(".");
+                        },
                         child: Text(".", style: TextStyle(fontSize: 35.0)),
                       ),
                     ),
